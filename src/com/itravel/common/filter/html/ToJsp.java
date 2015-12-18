e24a4e6c38a1cf2586ee45702f2c5ef0ca08706b
meikalong <meikalong>
2015-12-18 14:07:22 +0800

    移除创建html的类
    Signed-off-by: meikalong <meikalong>

--- src/com/itravel/common/filter/html/ToJsp.java
+++ src/com/itravel/common/filter/html/ToJsp.java
@@ -8,7 +8,6 @@ import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.bkweb.common.httpservlet.MyHttpServletResponseWrapper;
 import com.bkweb.common.utils.FileUtils;
 
-public class JumpToHtml implements Filter {
+public class ToJsp implements Filter {
 
 	@Override
 	public void destroy() {
@@ -26,25 +25,24 @@ public class JumpToHtml implements Filter {
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
 			ServletException {
-		String filePath = "C:\\Users\\Administrator\\Desktop\\1.html";
+		String filePath = "C:\\Users\\Administrator\\Desktop\\2.jsp";
 		FileUtils.createFile(filePath);
 		File file = new File(filePath);
-		FileOutputStream outputStream = new FileOutputStream(file);
+		FileOutputStream outputStream = new FileOutputStream(file, true);
 
 		HttpServletResponse response = (HttpServletResponse) res;
 
 		MyHttpServletResponseWrapper wrapper = new MyHttpServletResponseWrapper(response);
 		chain.doFilter(req, wrapper);
 
+		byte[] jsp = "<%@ page language=\"java\" import=\"java.util.*\" pageEncoding=\"UTF-8\"%>".getBytes();
 		byte[] bs = wrapper.getResponseData();
+		outputStream.write(jsp, 0, jsp.length);
 		outputStream.write(bs, 0, bs.length);
 		outputStream.flush();
 		outputStream.close();
-		ServletOutputStream output = response.getOutputStream();
-		String yes = "121212";
-		output.write(yes.getBytes());
-		output.flush();
-		output.close();
+
+		response.sendRedirect("http://www.baidu.com");
 	}
 
 	@Override
